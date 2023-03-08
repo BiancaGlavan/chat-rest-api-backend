30c8565e3a5cc00e4febc0533ffe996d0e4f0dd6
BiancaGlavan <bianca.glavan@yahoo.ro>
2023-03-08 15:41:30 +0200

    added auth routes

--- src/routes/authRoutes.ts
+++ src/routes/authRoutes.ts
@@ -1,5 +1,13 @@
 import express from "express";
+import { login, profile, register } from "../controllers/authController";
+import { isAuth } from "../middleware/authMiddleware";
 
 const router = express.Router();
+
+router.post('/register', register);
+router.post('/login', login);
+
+// add auth middleware to profile page. visible only when the user is logged in
+router.get('/profile', isAuth, profile);
 
 export default router;
